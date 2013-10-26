55c808401a1367d4011f9064eb84266ccbbebff9
Kim Birkelund <kim@birkelund.me>
2013-10-26 12:55:48 +0200

    Updated version.

--- src/Version.cs
+++ src/Version.cs
@@ -2,8 +2,6 @@ namespace Sekhmet.Xml.Parsing
 {
     internal static class Version
     {
-        public const string FullVersion = "0.1.0.0";
+        public const string FullVersion = "0.1.0.11";
     }
 }
-
-
